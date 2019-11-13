1a0faf51783ad7e062aa2c3bd9878af2afbd0b96
zainbinfurqan <zain.ahmed199524@gmail.com>
2019-11-13 23:48:36 +0500

    f

--- Connection/Connection.js
+++ Connection/Connection.js
@@ -4,10 +4,14 @@ var mysql = require('mysql');
 
 //local mysql db connection
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'movieplexdb'
+    // host     : 'localhost',
+    // user     : 'root',
+    // password : '',
+    // database : 'movieplexdb'
+    host : 'db4free.net',
+    user     : 'zainahmed',
+    password : 'pakistan@123',
+    database : 'savemoneydb'
 });
 
 connection.connect(function(err) {
